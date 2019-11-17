f251fff5fa02380d261be143906da3201dfc0c57
Warrior Gao <warrior.gao@gmail.com>
2019-11-17 19:12:16 +0800

    study es

--- spring-data-jpa/src/main/java/me/warriorg/spring/jpa/repository/CustomerRepository.java
+++ spring-data-jpa/src/main/java/me/warriorg/spring/jpa/repository/CustomerRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
+/**
+ * @author warrior
+ */
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
     List<Customer> findByLastName(String lastName);
 }
